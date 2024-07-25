0c8a709200357ca00f42e12bd2e71ad7f40d7dcb
MohamedMamluk <59979716+MohamedMamluk@users.noreply.github.com>
2024-07-25 20:21:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/pets-store/',
   plugins: [react()],
-
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true, // also necessary
+  },
   root: 'src',
 });
