8c9247c27c7cf8de70019c816fb3d9ca70b199e7
mohamedelsisi96 <mohamed.elsysey@gmail.com>
2024-07-25 19:35:55 +0300

    dist upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/pet-store/',
+  base: '/pets-store/',
   plugins: [react()],
 
   root: 'src',
